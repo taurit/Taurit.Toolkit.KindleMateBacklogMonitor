6e92a31f9ed00695926cf43a38f20dddbb9a7b70
taurit <sourcetree@taurit.pl>
2019-11-10 09:14:09 +0100

    Include timestamp in the console logs

--- Taurit.Toolkit.KindleMateBacklogMonitor/Program.cs
+++ Taurit.Toolkit.KindleMateBacklogMonitor/Program.cs
@@ -12,19 +12,21 @@ namespace Taurit.Toolkit.KindleMateBacklogMonitor
     /// </summary>
     internal class Program
     {
-        private readonly StatsReader _statsReader;
         private readonly ILogger _logger;
-        private StatsWriter _statsWriter;
+        private readonly StatsReader _statsReader;
+        private readonly StatsWriter _statsWriter;
 
         private Program()
         {
             // poor man's DI
             _statsReader = new StatsReader();
             _statsWriter = new StatsWriter();
-            _logger = LoggerFactory.Create(x => x.AddConsole()).CreateLogger(nameof(Program));
+            _logger = LoggerFactory.Create(x => x.AddConsole(
+                c => { c.TimestampFormat = "[HH:mm:ss] "; }
+            )).CreateLogger(nameof(Program));
         }
 
-        private static void Main(String[] args)
+        private static void Main(string[] args)
         {
             Parser.Default.ParseArguments<Args>(args).WithParsed(o => { new Program().Run(o); });
         }
@@ -34,24 +36,17 @@ namespace Taurit.Toolkit.KindleMateBacklogMonitor
             while (true) // for now it's designed to run on my Linux vm in an endless loop
             {
                 if (args.KindleMateDirectory is null)
-                {
                     args.KindleMateDirectory = "/KindleMate"; // expected mount point of a volume within the container
-                }
-                else
-                {
-                    // we still might want to allow to pass it as args to allow debugging on Windows (without containers)
-                }
+                // otherwise we still might want to allow to pass it as args to allow debugging on Windows (without containers)
 
                 if (!Directory.Exists(args.KindleMateDirectory))
-                {
                     throw new FileNotFoundException("Specified database file does not exist", args.KindleMateDirectory);
-                }
 
                 var dbFilePath = Path.Combine(args.KindleMateDirectory, "KM2.dat");
                 var statsFilePath = Path.Combine(args.KindleMateDirectory, "backlog-stats.csv");
 
                 _logger.Log(LogLevel.Information, "Reading stats from the database...");
-                Stats currentStats = _statsReader.ReadStats(dbFilePath);
+                var currentStats = _statsReader.ReadStats(dbFilePath);
 
                 _logger.Log(LogLevel.Information, "Appending stats to an output file...");
                 _statsWriter.AppendStats(statsFilePath, currentStats);
